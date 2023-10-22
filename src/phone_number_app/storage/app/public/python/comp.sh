ad17c1c4dec8c2bd96eea423fd951d394176b9c7
wataruapacies <ctwj0502@mail4.doshisha.ac.jp>
2023-10-22 15:46:23 +0900

    流石に修正した

--- src/phone_number_app/storage/app/public/python/comp.sh
+++ src/phone_number_app/storage/app/public/python/comp.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-eval "python3 /var/www/html/phone_number_app/storage/app/public/python/i_town_comp01.py"
+eval "python3 /var/www/html/phone_number_app/storage/app/public/python/reqcsv.py"
